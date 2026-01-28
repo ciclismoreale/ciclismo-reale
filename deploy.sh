ed14cc96953ad66e556775899e5139a924142aa5
Thomas Robiglio <robigliothomas@gmail.com>
2026-01-28 15:00:48 +0100

    changed formatting budget

--- deploy.sh
+++ deploy.sh
@@ -2,6 +2,7 @@ python code/scrape_cq_ranking.py
 python code/fill_ranking_page.py
 python code/fill_teams_page.py
 python code/rss_news.py
+python code/fill_monthly.py
 
 date -u +"Ultimo aggiornamento: %d-%m-%Y %H:%M:%S UTC" > last_updated.md
 quarto render
